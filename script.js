cf72814bfb7e99237c9c03350a3855d21e72bc30
kosmossierraflaca-wq <kosmossierraflaca@gmail.com>
2025-10-09 11:32:37 -0600

    cambios de diseno, agregar pagina de catalogo, estilo de catalogo, js de catalogo,

---
+++ script.js
@@ -0,0 +1,30 @@
+let productos = [];
+
+function agregarProducto(imagen, descripcion, precio) {
+    productos.push({
+        imagen: imagen,
+        descripcion: descripcion,
+        precio: precio
+    });
+    productos.sort((a, b) => a.descripcion.localeCompare(b.descripcion));
+    mostrarProductos();
+}
+
+function mostrarProductos() {
+    let contenedor = document.getElementById('productos');
+    contenedor.innerHTML = '';
+    productos.forEach(producto => {
+        let elemento = document.createElement('div');
+        elemento.classList.add('producto');
+        elemento.innerHTML = `
+            <img src="${producto.imagen}" alt="${producto.descripcion}">
+            <div class="descripcion">${producto.descripcion}</div>
+            <div class="precio">${producto.precio}</div>
+        `;
+        contenedor.appendChild(elemento);
+    });
+}
+
+// Agregar productos
+agregarProducto('/imagenes/senalsalidaemer.png', 'Señal de Emergencia 1', '$30.00');
+agregarProducto('/imagenes/bacoiris12pz.png', 'Colores Bacoiris 12 pzas', '$44.00');
