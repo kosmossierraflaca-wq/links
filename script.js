5335eeac018614183ea92ffbe0e512705c166240
Benito Mendez <benedictwheatley@gmail.com>
2025-10-09 19:22:45 -0600

    agregar productos

--- script.js
+++ script.js
@@ -28,3 +28,9 @@ function mostrarProductos() {
 // Agregar productos
 agregarProducto("./imagenes/senalsalidaemer.png", 'Señal de Emergencia 1', '$30.00');
 agregarProducto("./imagenes/bacoiris12pz.png", 'Colores Bacoiris 12 pzas', '$44.00');
+agregarProducto("./imagenes/cartulinanegra.png", 'Cartulina Negra Pliego 1pza', '$10.00');
+agregarProducto("./imagenes/cartulinablanca.png", 'Cartulina Blanca Pliego 1pza', '$5.00');
+agregarProducto("./imagenes/gomafactiss20.png", 'Goma Factis S20 1pza', '$9.00');
+agregarProducto("./imagenes/gomafactis36r.png", 'Goma Factis 36r 1pza', '$4.00');
+agregarProducto("./imagenes/cutterchicocolor.png", 'Cutter Chico Color 1pz', '$2.00');
+agregarProducto("./imagenes/cuttergrandecolor.png", 'Cutter Grande Color 1pz', '$4.00');
