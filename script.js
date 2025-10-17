2cc1ea2d045c952ee7656c750c29748f83e74d1d
Benito Mendez <benedictwheatley@gmail.com>
2025-10-16 19:03:06 -0600

    correccion de disenio

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
+
 let productos = [];
 
-function agregarProducto(imagen, descripcion, precio) {
+function agregarProducto(imagen, descripcion) {
     productos.push({
         imagen: imagen,
-        descripcion: descripcion,
-        precio: precio
+        descripcion: descripcion
     });
     productos.sort((a, b) => a.descripcion.localeCompare(b.descripcion));
     mostrarProductos();
@@ -19,18 +19,18 @@ function mostrarProductos() {
         elemento.innerHTML = `
             <img src="${producto.imagen}" alt="${producto.descripcion}">
             <div class="descripcion">${producto.descripcion}</div>
-            <div class="precio">${producto.precio}</div>
         `;
         contenedor.appendChild(elemento);
     });
 }
 
 // Agregar productos
-agregarProducto("./imagenes/senalsalidaemer.png", 'Señal de Emergencia 1', '$30.00');
-agregarProducto("./imagenes/bacoiris12pz.png", 'Colores Bacoiris 12 pzas', '$44.00');
-agregarProducto("./imagenes/cartulinanegra.png", 'Cartulina Negra Pliego 1pza', '$10.00');
-agregarProducto("./imagenes/cartulinablanca.png", 'Cartulina Blanca Pliego 1pza', '$5.00');
-agregarProducto("./imagenes/gomafactiss20.png", 'Goma Factis S20 1pza', '$9.00');
-agregarProducto("./imagenes/gomafactis36r.png", 'Goma Factis 36r 1pza', '$4.00');
-agregarProducto("./imagenes/cutterchicocolor.png", 'Cutter Chico Color 1pz', '$2.00');
-agregarProducto("./imagenes/cuttergrandecolor.png", 'Cutter Grande Color 1pz', '$4.00');
+agregarProducto("./imagenes/senalsalidaemer.png", 'Señal de Emergencia $30.00');
+agregarProducto("./imagenes/bacoiris12pz.png", 'Colores Bacoiris 12 pzas $44.00');
+agregarProducto("./imagenes/cartulinanegra.png", 'Cartulina Negra Pliego 1pza $10.00');
+agregarProducto("./imagenes/cartulinablanca.png", 'Cartulina Blanca Pliego 1pza $5.00');
+agregarProducto("./imagenes/gomafactiss20.png", 'Goma Factis S20 1pza $9.00');
+agregarProducto("./imagenes/gomafactis36r.png", 'Goma Factis 36r 1pza $4.00');
+agregarProducto("./imagenes/cutterchicocolor.png", 'Cutter Chico Color 1pz $2.00');
+agregarProducto("./imagenes/cuttergrandecolor.png", 'Cutter Grande Color 1pz $4.00');
+
